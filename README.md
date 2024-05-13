ad0c0dde08f3967a0fa9d0472b22f393aa898a54
Olevonk <olevonk@gmail.com>
2024-05-13 11:11:03 -0500

    Feature jQuery Dropdown Menu

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# jQuery-Dropdown-Menu
+# jQuery Dropdown Menu
 Implementing a dropdown menu feature using jQuery on the Wildlife Sea Cruises website to navigate to different pages.
--- project12-01.js
+++ project12-01.js
@@ -4,15 +4,19 @@
       Project 12-01
 
       Project to display a dropdown menu
-      Author: 
-      Date:   
+      Author: Oleksii Fon Krupp
+      Date:   05/13/2024
 
       Filename: project12-01.js
 */
 
-
-
-
-
-
-                                                
+// Show dropdown menu on mouseover and hide on mouseout for submenu items
+$(document).ready(function() {
+      $("li.submenu").mouseover(function(e) {
+          $(e.currentTarget).children("ul").show();
+      });
+  
+      $("li.submenu").mouseout(function(e) {
+          $(e.currentTarget).children("ul").hide();
+      });
+  });                   
